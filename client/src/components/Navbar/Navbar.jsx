354c1b6d9d92a1111716c902337115a409f04285
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-12-21 16:16:48 +0400

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 export const Navbar = () => {
     return (
         <div className="navbar">
-            <img className="ieee-logo-white" alt="IEEE Logo White" src="IEEE-logo-white.png" />
+            <img className="ieee-logo-white" alt="IEEE Logo White" src="/assets/IEEE_Logo_White.png" />
             <div className="frame">
                 <div className="text-wrapper">Home</div>
                 <div className="text-wrapper">Contact</div>
