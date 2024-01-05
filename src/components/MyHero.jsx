06aeadb1a23c152295756dc6807ae9ac4f24b85f
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-01-06 00:25:42 +0530

    Create MyHero.jsx

---
+++ src/components/MyHero.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const MyHero = () => {
+    return (
+        <section className="bg-white mt-12">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+                    Welcome to IEEE SRM Event Registration
+                </h1>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
+                    Unlock Exclusive Access to Exciting Events and Workshops
+                </p>
+                <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                    <a
+                        href="#"
+                        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-800 hover:bg-gray-500 focus:ring-4 focus:ring-primary-300"
+                    >
+                        Register Here
+                        <svg
+                            className="ml-2 -mr-1 w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MyHero;
