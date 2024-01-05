ae72b7ad64e8f8fc040085c44898b1807276e820
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-01-06 00:58:51 +0530

    Update MyHero.jsx

--- src/components/MyHero.jsx
+++ src/components/MyHero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const MyHero = () => {
     return (
+
         <section className="bg-white mt-12">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
@@ -10,7 +11,7 @@ const MyHero = () => {
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
                     Unlock Exclusive Access to Exciting Events and Workshops
                 </p>
-                <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                <div className="flex flex-col mb-2 lg:mb-2 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     <a
                         href="#"
                         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-800 hover:bg-gray-500 focus:ring-4 focus:ring-primary-300"
@@ -32,6 +33,7 @@ const MyHero = () => {
                 </div>
             </div>
         </section>
+
     );
 };
 
