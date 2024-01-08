86764b1b37a6c28a56e577be6a101c3c5f901bc8
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-01-08 16:13:50 +0530

    Create firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig from './firebaseConfig';
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
