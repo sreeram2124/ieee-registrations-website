d20d73d3280df94c82de5abd9ba915042ca1cd4f
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-01-08 16:13:42 +0530

    Update MyNavbar.jsx

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navbar } from 'flowbite-react';
+import PdfGeneratorButton from '../utils/PdfGeneratorButton';
 
 const MyNavbar = () => {
     return (
-
         <Navbar fluid rounded>
             <Navbar.Brand href="#">
                 <img src="/img/IEEE_SRM_SB.png" className="mr-3 h-6 sm:h-0 md:h-9 lg:h-12" alt="IEEE SRM" />
@@ -15,13 +15,9 @@ const MyNavbar = () => {
                 <Navbar.Link href="https://ieeesrmist.com" className="hover:underline md:hover:text-blue-800">
                     Home
                 </Navbar.Link>
-                <Navbar.Link href="#" className="hover:underline md:hover:text-blue-800">
-                    Login
-                </Navbar.Link>
+                <PdfGeneratorButton />
             </Navbar.Collapse>
-
         </Navbar>
-
     );
 };
 
